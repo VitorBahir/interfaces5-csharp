19352cb2b8957afa6a33e06c4c2732736e44aa2f
Vitor Bahir <vitorb82014@gmail.com>
2023-03-26 20:22:34 -0300

    Using IComparable interface

---
+++ Course/Program.cs
@@ -0,0 +1,31 @@
+﻿using Course.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"C:\temp\projetos\interfaces5-csharp\in.txt";
+
+        try
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                List<Employee> list = new List<Employee>();
+                while (!sr.EndOfStream)
+                {
+                    list.Add(new Employee(sr.ReadLine()));
+                }
+                list.Sort();
+                foreach (Employee e in list)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+        catch (IOException e)
+        {   
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
